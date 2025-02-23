f435da5b5ca94e8b180c22a26a88ef3f7c3b606a
Tyler Lartonoix <lord.codemonkey@gmail.com>
2025-02-23 18:39:35 -0500

    Add build and deploy scripts

--- bin/build.bat
+++ bin/build.bat
@@ -1,1 +1,6 @@
-docker run --rm --name mhdb-ID -v %~dp0\..\build:/srv/slate/build -v %dp0\..\source:/srv/slate/source slatedocs/slate build
+docker run --rm --name mhdb-docs -p 4567:4567 ^
+    -v %~dp0\..\source:/srv/slate/source ^
+    -v %~dp0\..\lib:/srv/slate/lib ^
+    -v %~dp0\..\build:/srv/slate/build ^
+    --mount type=bind,src=%~dp0\..\config.rb,dst=/srv/slate/config.rb ^
+    slatedocs/slate build
--- bin/deploy.bat
+++
@@ -1,5 +0,0 @@
-docker run --rm --name mhdb-docs -p 4567:4567 ^
-    -v %~dp0\..\source:/srv/slate/source ^
-    -v %~dp0\..\lib:/srv/slate/lib ^
-    --mount type=bind,src=%~dp0\..\config.rb,dst=/srv/slate/config.rb ^
-    slatedocs/slate deploy
